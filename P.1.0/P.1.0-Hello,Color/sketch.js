674a69a1046c1b4311063b00cf1230d2cdc8eb90
GoodGuyGregory <greg.witt625@gmail.com>
2021-08-12 22:00:42 -0400

    adds Hello Color

--- P.1.0/P.1.0-Hello,Color/sketch.js
+++ P.1.0/P.1.0-Hello,Color/sketch.js
@@ -1,10 +1,19 @@
 function setup() {
-    createCanvas(720, 720);
+    createCanvas(windowWidth, windowHeight);
     noCursor();
 
-    //  
+    //  HSB 
+    colorMode(HSB, 360, 100, 100);
+    rectMode(CENTER);
+    noStroke();
 }
 
 function draw() {
+    //  establish the background from mouseY
+    background(mouseY / 2, 100, 100);
+
+    // goes the opposite way of the background manipulations
+    fill(360 - mouseY / 2, 100, 100);
+    rect(windowWidth / 2, windowHeight / 2, mouseX + 1, mouseX + 1);
 
 }
